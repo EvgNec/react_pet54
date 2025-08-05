ec3e230dd58cecd597c1df960192ef82dc1e9ffe
Evgeniy <evgnec@gmail.com>
2025-08-06 00:15:56 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { Container, Header, Link } from './App.styled';
 import { Routes, Route } from 'react-router-dom'
 
 import Home from '../pages/Home';
-import ToDo from '../pages/ToDo';
+import ToDoList from '../pages/ToDoList';
 import NotFound from '../pages/NotFound';
 
 const App = () => {
@@ -16,7 +16,7 @@ const App = () => {
       </Header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="todo" element={<ToDo />}>        
+        <Route path="todo" element={<ToDoList />}>        
 
           
         </Route>
---
+++ src/pages/ToDoList.jsx
@@ -0,0 +1,12 @@
+import ToDo from 'components/ToDo/ToDo'
+import React from 'react'
+
+const ToDoList = () => {
+  return (
+    <div>
+      <ToDo/>
+    </div>
+  )
+}
+
+export default ToDoList

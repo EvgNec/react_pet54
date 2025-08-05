83efe0176bbd3e37c6c8f33986fdef76dd9f97fa
Evgeniy <evgnec@gmail.com>
2025-08-05 20:27:11 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Routes, Route } from 'react-router-dom'
+import Layout from './Layout/Layout'
+
+import ToDoPage from './pages/ToDoPage'
+import ToDoDetails from './ToDo/ToDoDetails'
+
+const App = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route path='todo' element={<ToDoPage />} />
+				<Route path='todo/:id/:title' element={<ToDoDetails />} />
+			</Route>
+		</Routes>
+	)
+}
+
+export default App
